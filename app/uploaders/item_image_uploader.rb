f0dc0c53a121c9285a8cf28c6b44dfb1b5ae1ff5
m.taira <taira.masaru@gmail.com>
2014-12-20 19:58:03 +0900

    アップロードした画像のリサイズ

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -31,9 +31,12 @@ class ItemImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  # 画像の上限を700pxにする
+  process :resize_and_pad => [450, 450]
+
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_and_pad => [100, 100]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
